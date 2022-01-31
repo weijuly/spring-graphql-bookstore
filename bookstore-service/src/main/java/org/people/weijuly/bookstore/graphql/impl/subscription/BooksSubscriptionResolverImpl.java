9c0e06ac9ecf5b8cf30fc1ce450f4a97c7a63ce1
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2022-01-31 15:03:40 +0530

    configure graphql-java-codegen to generate subscription return types

--- bookstore-service/src/main/java/org/people/weijuly/bookstore/graphql/impl/subscription/BooksSubscriptionResolverImpl.java
+++ bookstore-service/src/main/java/org/people/weijuly/bookstore/graphql/impl/subscription/BooksSubscriptionResolverImpl.java
@@ -1,6 +1,7 @@
 package org.people.weijuly.bookstore.graphql.impl.subscription;
 
 import org.people.weijuly.bookstore.model.BookModel;
+import org.people.weijuly.bookstore.model.BooksSubscriptionResolver;
 import org.people.weijuly.bookstore.service.BookService;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,13 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 
 @Component
-public class BooksSubscriptionResolverImpl {
+public class BooksSubscriptionResolverImpl implements BooksSubscriptionResolver {
 
     @Autowired
     BookService bookService;
 
     public Publisher<BookModel> books() throws Exception {
-        return Flux.zip(
-                Flux.interval(Duration.ofSeconds(1L)),
-                Flux.fromStream(bookService.allBooks()),
-                (x, y) -> y
-        );
+        return Flux.interval(Duration.ofSeconds(1L))
+                .zipWith(Flux.fromStream(bookService.allBooks()), (x, y) -> y);
     }
 }
