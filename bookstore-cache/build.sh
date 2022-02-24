5f3d2a1c14d8cc51a7bea98c0f32d99f125da4ee
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2022-02-24 11:17:26 +0530

    add ghcr support

--- bookstore-cache/build.sh
+++ bookstore-cache/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TIMESTAMP=`date '+%Y%m%d%H%M%S'`
-echo "Building image: bookstore-cache:0.0.1-${TIMESTAMP}"
-docker build -t bookstore-cache:0.0.1-${TIMESTAMP} .
+echo "Building image: ghcr.io/weijuly/bookstore-cache:0.0.1-${TIMESTAMP}"
+docker build -t ghcr.io/weijuly/bookstore-cache:0.0.1-${TIMESTAMP} .
--- bookstore-gateway/build.sh
+++ bookstore-gateway/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TIMESTAMP=`date '+%Y%m%d%H%M%S'`
-echo "Building image: bookstore-gateway:0.0.1-${TIMESTAMP}"
-docker build -t bookstore-gateway:0.0.1-${TIMESTAMP} .
+echo "Building image: ghcr.io/weijuly/bookstore-gateway:0.0.1-${TIMESTAMP}"
+docker build -t ghcr.io/weijuly/bookstore-gateway:0.0.1-${TIMESTAMP} .
